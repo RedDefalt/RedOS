07d6dee627ee8260ae2a5b3b13ba69735c799777
RedDefalt <mirzoyanhrayr28@gmail.com>
2024-03-22 16:35:51 +0100

    Creation of struct ObjectData in DataStructure.h

--- inc/DataStructure.h
+++ inc/DataStructure.h
@@ -5,6 +5,12 @@
 #ifndef DATASTRUCTURE_H
 #define DATASTRUCTURE_H
 
+typedef struct ObjectData{
+    char name[127];
+    int file;
+    int *dataptr[50];
+    int number_of_ptr;
+} StructureObjectData;
 
 
 #endif //DATASTRUCTURE_H
