f8909c0ca3fc947f0a61fd7fa74cf751ff2d9031
RedDefalt <mirzoyanhrayr28@gmail.com>
2024-03-24 16:56:43 +0100

    update main.C as prototype

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
+/**
+ * @file main.c
+ * @author Hrayr Mirzoyan
+ * @brief Main file of RedOS
+ */
+
 #include <stdio.h>
 #include "../inc/DataStructure.h"
 
-int main(void)
-{
-    printf("Hello, World!\n");
+int main(void) {
+    StructureObjectData* root = InitRoot();
+    MakeDirectory(root, "Bonjour");
+    ListDirectory(root);
+    MakeDirectory(root, "Yasmine");
+    ListDirectory(root);
     return 0;
 }
